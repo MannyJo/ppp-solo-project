984870b5310a11ea8f997b470dbb73992be25949
Manny Jo <jomansang@gmail.com>
2019-07-31 16:10:55 -0500

    .

---
+++ build/precache-manifest.d01ece48b056aa46d4eadaf30e7b5560.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "125a9da729e8dee2a43116034c2e18fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9beadc16e79e55967f0",
+    "url": "/static/css/main.2eaf6d16.chunk.css"
+  },
+  {
+    "revision": "d5396b657e5c29da20f7",
+    "url": "/static/js/2.9d84eefa.chunk.js"
+  },
+  {
+    "revision": "a9beadc16e79e55967f0",
+    "url": "/static/js/main.1b08098e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ src/constants/constants.js
@@ -0,0 +1,5 @@
+const constants = {
+    API_KEY: 'AIzaSyA8ALSMNJnujiOFPjfzNmT8CzBEVdqIsj4',
+};
+
+export default constants;
